aad1181e08fb507e4922d51fcc53de9e10060e44
Lohidhasan <lohi.s@unitedhands.cc>
2023-04-23 10:39:20 +0530

    update content

--- src/Content/Home.js
+++ src/Content/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "../App.css";
+import dev from "../Assets/Dev1.png";
+import dev1 from "../Assets/Dev2.png";
 const Home = () => {
   return (
     <div>
@@ -9,13 +11,43 @@ const Home = () => {
         for the need to automate and secure the whole software development
         lifecycle.
       </p>
-
+      <img src={dev} alt="img" />
       <p>
         Continuous integration (CI) and continuous delivery/deployment (CD) are
         DevOps processes that help to automate the software release process in a
         way that guarantees more seamless deployments. They also save time and
         money for an organization.
       </p>
+      <h1>Phases in a CI/CD pipeline</h1>
+      <img src={dev1} alt="" />
+      <ul>
+        <li>
+          <b>The build stage: </b>
+          This is where the source code and its dependencies are bundled. The
+          primary purpose of the build stage is to package the application to be
+          easily deployed to different environments
+        </li>
+        <li>
+          <b>The test stage: </b>
+          This is where automated test cases are executed to verify the
+          correctness and behavior of the changes that are being introduced.
+        </li>
+        <li>
+          <b>The release stage: </b>
+          This is where the built code is stored in a redistributed format in an
+          artifact repository.
+        </li>
+        <li>
+          <b>The deploy stage: </b>
+          This is where the build artifact is picked up and deployed to the
+          required environment.
+        </li>
+        <li>
+          <b>The monitor stage:</b>
+          This is the stage where the overall application performance is
+          monitored using a dedicated interface.
+        </li>
+      </ul>
     </div>
   );
 };
